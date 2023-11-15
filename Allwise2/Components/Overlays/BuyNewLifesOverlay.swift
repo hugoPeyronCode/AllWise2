cac7e4148bf6cf6456b2d53a0de273652ad69d88
Hugo Peyron <hugopeyron@gmail.com>
2023-11-15 16:36:56 +0100

    Premium and life mechanic wip

---
+++ Allwise2/Components/Overlays/BuyNewLifesOverlay.swift
@@ -0,0 +1,18 @@
+//
+//  BuyNewLifesOverlay.swift
+//  Allwise2
+//
+//  Created by Hugo Peyron on 15/11/2023.
+//
+
+import SwiftUI
+
+struct BuyNewLifesOverlay: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BuyNewLifesOverlay()
+}
