40366345a684209dfbcc3a475d857fda93e2d653
Hugo Peyron <hugopeyron@gmail.com>
2023-11-21 11:41:19 +0100

    Latest commit

--- Allwise2/Core/Questions/QCM/View/QuestionQCMView.swift
+++ Allwise2/Core/Questions/QCM/View/QuestionQCMView.swift
@@ -44,7 +44,6 @@ struct QuestionQCMView: View {
                 ValidationButton(questionState: localVM.questionState) {
                         validationButtonActions()
                     }
-
             }
         }
     }
