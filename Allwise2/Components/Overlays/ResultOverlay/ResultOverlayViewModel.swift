75cb58bc9decb6bb4292c259dc00eb3f7fe31f84
Hugo Peyron <hugopeyron@gmail.com>
2023-11-21 11:33:48 +0100

    Created resultOverlay ViewModel

---
+++ Allwise2/Components/Overlays/ResultOverlay/ResultOverlayViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ResultOverlayViewModel.swift
+//  Allwise2
+//
+//  Created by Hugo Peyron on 21/11/2023.
+//
+
+import Foundation
+import SwiftUI
+
+class ResultOverlayViewModel : ObservableObject {
+    
+}
