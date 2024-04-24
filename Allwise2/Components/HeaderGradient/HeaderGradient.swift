276d775f4b41b097f221df9e20432a3c9abb3adf
hugoPeyronCode <110022126+hugoPeyronCode@users.noreply.github.com>
2024-04-24 17:18:37 +0200

    New Art Direction try on the Meditation View.

---
+++ Allwise2/Components/HeaderGradient/HeaderGradient.swift
@@ -0,0 +1,18 @@
+//
+//  HeaderGradient.swift
+//  Allwise2
+//
+//  Created by Hugo Peyron on 24/04/2024.
+//
+
+import SwiftUI
+
+struct HeaderGradient: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HeaderGradient()
+}
---
+++ Allwise2/Components/VideoPlayer/VideoPlayerView.swift
@@ -0,0 +1,8 @@
+//
+//  VideoPlayer.swift
+//  Allwise2
+//
+//  Created by Hugo Peyron on 24/04/2024.
+//
+
+import Foundation
